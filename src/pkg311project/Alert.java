c51f220ca2466f80b603397aef8fd5ad40cfcc1c
DeeShalz <DeeShalz@client-104-39-93-226.mobility.psu.edu>
2017-02-16 14:02:57 -0500

    First

---
+++ src/pkg311project/Alert.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg311project;
+
+/**
+ *
+ * @author DeeShalz
+ */
+public class Alert {
+    
+}
